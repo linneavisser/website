34243d09d552c6d92c29115f318443bcbff57f1e
Thordis Kara Valsdottir <thor9706@stud.kea.dk>
2022-04-28 16:13:02 +0200

    working js and template

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ const navbar = document.querySelector(".navbar");
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("change");
 });
+
+// PRODUCT LIST
+window.addEventListener("DOMContentLoaded", init);
+
+function init(event) {
+  getData();
+}
+
+async function getData() {
+  let result = await fetch(
+    "https://thordiskara.com/wp_silfen/wp-json/wp/v2/bag?_embed"
+  );
+  showBag(await result.json());
+}
+
+function showBag(bagArray) {
+  console.log(bagArray);
+  const template = document.querySelector(".productlistTemplate").content;
+  const parentElement = document.querySelector(".productlistmain");
+  bagArray.forEach((bag) => {
+    const copy = template.cloneNode(true);
+    copy.querySelector(".bagName").textContent = bag.title.rendered;
+    copy.querySelector(".price span").textContent = bag.price;
+    copy.querySelector(".bagImg").src =
+      bag._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url;
+    parentElement.appendChild(copy);
+  });
+}
