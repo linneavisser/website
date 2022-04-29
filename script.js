10ead3dfb6b387c3824b87c7d16d404a3e1e161b
linneavisser <anna56c1@stud.kea.dk>
2022-04-29 10:52:54 +0200

    product view

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const navbar = document.querySelector(".navbar");
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("change");
 });
+
+//PLUS SIGN and Bag info
+const plusIcon = document.querySelector(".plusSign");
+const toggleInfo = document.querySelector(".toggleInner");
+
+plusIcon.addEventListener("click", () => {
+  console.log(toggleInfo);
+  toggleInfo.classList.toggle("change");
+});
